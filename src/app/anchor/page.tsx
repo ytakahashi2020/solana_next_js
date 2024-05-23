8f6b364c8868d05d1cca3a2f3f5cc37b691ca12b
ytakahashi2020 <y.takahashi2020@gmail.com>
2024-05-23 12:50:45 +0900

    署名が必要なコントラクトの実行

--- src/app/anchor/page.tsx
+++ src/app/anchor/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { callHelloProgram } from "../../anchorClient";
+import { callHelloProgram2 } from "../../anchorClient2";
 import {
   useConnection,
   useWallet,
@@ -32,6 +33,22 @@ const AnchorPage: React.FC = () => {
       setStatus(`プログラムの実行に失敗しました: ${err.message}`);
     }
   };
+  const handleHelloClick2 = async () => {
+    if (!connected) {
+      setStatus("ウォレットが接続されていません");
+      return;
+    }
+
+    setStatus("プログラム実行中...");
+
+    try {
+      const result = await callHelloProgram2(wallet, connection);
+      setStatus("プログラムが正常に実行されました");
+      setResultUrl(`https://solscan.io/tx/${result}?cluster=devnet`);
+    } catch (err: any) {
+      setStatus(`プログラムの実行に失敗しました: ${err.message}`);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
@@ -42,6 +59,41 @@ const AnchorPage: React.FC = () => {
         {/* <WalletMultiButton className="mb-4" /> */}
         <button
           onClick={handleHelloClick}
+          disabled={!connected}
+          className="relative w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Helloプログラムを実行
+        </button>
+      </div>
+      {status && (
+        <div className="mt-4 p-2 bg-gray-200 rounded-md shadow-inner max-w-md w-full overflow-auto">
+          <p className="text-center text-sm text-gray-600 break-all">
+            {status}
+          </p>
+        </div>
+      )}
+      {resultUrl && (
+        <div className="mt-4 p-2  max-w-md w-full overflow-auto">
+          <p className="text-left text-sm text-gray-600 break-all">
+            <p className="text-left">実行結果</p>
+            <a
+              href={resultUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600"
+            >
+              {resultUrl}
+            </a>
+          </p>
+        </div>
+      )}
+      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-semibold mb-6 text-center">
+          Anchorプログラム実行
+        </h1>
+        {/* <WalletMultiButton className="mb-4" /> */}
+        <button
+          onClick={handleHelloClick2}
           disabled={!connected}
           className="relative w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         >
